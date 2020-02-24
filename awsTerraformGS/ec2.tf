22060d218822be709aafb178c52e93e10680add9
Adam Ayd <adam.ayd@gmail.com>
2020-02-23 21:26:13 -0500

    created EC2 isntance with terraform

---
+++ awsTerraformGS/ec2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "gs-example" {
+  ami           = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+}
